e0f053e4048a81264aaff2a4525910e803645027
Kacper Walanus <kacper@walanus.com>
2017-06-19 08:02:51 +0200

    expect #call to be implemented

--- lib/coman/command.rb
+++ lib/coman/command.rb
@@ -2,17 +2,27 @@ module Coman
   module Command
     require_relative 'command/command_errors'
 
+    CALL_METHOD_NAME = :call.freeze; private_constant :CALL_METHOD_NAME
     RESPONSE_METHOD_NAME = :response.freeze; private_constant :RESPONSE_METHOD_NAME
 
     def self.included(base)
-      if base.instance_methods.include?(RESPONSE_METHOD_NAME)
+      instance_methods = base.instance_methods
+
+      if instance_methods.include?(RESPONSE_METHOD_NAME)
         raise CannotOverrideMethod.new(extended_class:       base,
                                        reserved_method_name: RESPONSE_METHOD_NAME)
       end
 
-      define_method RESPONSE_METHOD_NAME do
-
+      unless instance_methods.include?(CALL_METHOD_NAME)
+        raise NotImplementedError, "#{Coman::Command} expects #{base} to implement ##{CALL_METHOD_NAME}"
       end
+
+      define_method RESPONSE_METHOD_NAME do
+      end
+    end
+
+    def self.extended(instance)
+      included(instance.class)
     end
   end
 end
--- spec/coman/command_spec.rb
+++ spec/coman/command_spec.rb
@@ -1,21 +1,58 @@
 require_relative '../../lib/coman/command'
 
 RSpec.describe Coman::Command do
-  let(:extended_class) { Class.new.include(described_class) }
-  let(:extended_instance) { extended_class.new }
+  describe 'extended instance' do
+    subject { class_to_extend.new.extend(described_class) }
 
-  it { expect(extended_instance).to respond_to(:response) }
-
-  context 'when extended instance already implements #response method' do
-    let(:extended_instance) do
-      extended_class.new.tap do
-        def response
+    let(:class_to_extend) do
+      Class.new do
+        def call
         end
       end
     end
 
-    it do
-      expect{ extended_instance }.to raise_error(described_class::CannotOverrideMethod)
+    it { expect(subject.public_methods).to include(:response) }
+
+    context 'when already implements #response' do
+      let(:class_to_extend) do
+        Class.new do
+          def response
+          end
+        end
+      end
+
+      it do
+        expect { subject }.to raise_error(described_class::CannotOverrideMethod)
+      end
+    end
+  end
+
+  describe '#response' do
+    subject do
+      class_to_extend.new.extend(described_class).response
+    end
+
+    context 'when extended class' do
+      context 'does not implement #call' do
+        let(:class_to_extend) { Object }
+
+        it do
+          error_message = 'Coman::Command expects Object to implement #call'
+          error_class   = NotImplementedError
+          expect { subject }.to raise_error(error_class, error_message)
+        end
+      end
+
+      context 'implements #call' do
+        let(:class_to_extend) do
+          Class.new do
+            def call
+            end
+          end
+        end
+
+        it { expect { subject }.to_not raise_error }
+      end
     end
   end
 end
